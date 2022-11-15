84b2316ed8c4b7da0663e5fdd4839de0a34b39aa
mjz <mjz@sfu.ca>
2022-11-15 04:31:38 -0800

    added methods to display data as described by transaction

--- app/src/main/java/com/group19/stashup/ui/transactions/ViewTransactionFragment.kt
+++ app/src/main/java/com/group19/stashup/ui/transactions/ViewTransactionFragment.kt
@@ -1,29 +1,115 @@
 package com.group19.stashup.ui.transactions
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceManager
+import com.group19.stashup.R
 import com.group19.stashup.databinding.FragmentViewTransactionBinding
+import com.group19.stashup.ui.transactions.database.PersonListViewAdapter
+import com.group19.stashup.ui.transactions.database.Transaction
+import com.group19.stashup.ui.transactions.database.TransactionsViewModel
 
-class ViewTransactionFragment : Fragment() {
+class ViewTransactionFragment : Fragment(), View.OnClickListener {
 
+    // Binding
     private var _binding: FragmentViewTransactionBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var transaction: Transaction
+    private lateinit var transactionsViewModel: TransactionsViewModel
+    private lateinit var people: ArrayList<String>
+
+    @Suppress("DEPRECATION")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentViewTransactionBinding.inflate(inflater, container, false)
+        transaction = requireArguments().getParcelable("transaction")!!
+
+        transactionsViewModel = ViewModelProvider(this)[TransactionsViewModel::class.java]
+
+        setViewsInLayout()
+
         return binding.root
+    }
+
+    /**
+     * Set data as described by [transaction].
+     */
+    private fun setViewsInLayout() {
+        val preference = PreferenceManager.getDefaultSharedPreferences(requireActivity())
+        val currencyCode = preference.getString("currency", "CAD")
+
+        binding.deleteBtn.setOnClickListener(this)
+        binding.addPersonBtn.setOnClickListener(this)
+
+        binding.transactionNameTv.text = transaction.transactionName
+        var cost = "-$ ${String.format("%.2f", transaction.cost)} $currencyCode"
+        if (transaction.ownerUid == transaction.payerUid) {
+            binding.totalCostTv.setTextColor(requireActivity().getColor(R.color.green))
+            cost = "+$ ${String.format("%.2f", transaction.cost)} $currencyCode"
+        }
+
+        binding.totalCostTv.text = cost
+        if (!transaction.isShared) {
+            binding.listViewCv.visibility = Button.INVISIBLE
+            binding.addPersonBtn.visibility = Button.INVISIBLE
+        }
+
+        people = transaction.people
+        val listAdapter = PersonListViewAdapter(people, transaction, requireActivity())
+        binding.listView.adapter = listAdapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        if (v == null) return
+
+        when (v.id) {
+            R.id.delete_btn -> {
+                transactionsViewModel.deleteEntry(transaction.transactionUid)
+                findNavController().popBackStack()
+            }
+            R.id.add_person_btn -> {
+                onAddPersonClicked()
+            }
+        }
+    }
+
+    /**
+     * On add person clicked, add to list view and to realtime database.
+     */
+    private fun onAddPersonClicked() {
+        val builder = AlertDialog.Builder(requireActivity())
+
+        val editText = EditText(requireActivity())
+        builder.apply {
+            setTitle("Enter the name:")
+            setView(editText)
+            setPositiveButton("OK") { _: DialogInterface, _: Int ->
+                people.add(editText.text.toString())
+                val listAdapter = PersonListViewAdapter(people, transaction, requireActivity())
+                binding.listView.adapter = listAdapter
+                transactionsViewModel.updatePeople(
+                    transaction.transactionUid,
+                    editText.text.toString()
+                )
+            }
+            setNegativeButton("CANCEL") { _: DialogInterface, _: Int -> }
+        }
+        builder.create().show()
     }
 }
