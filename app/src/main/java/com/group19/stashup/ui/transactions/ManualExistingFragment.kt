00235e24324eeca8a85f87b342f64fa9b02137e3
mjz <mjz@sfu.ca>
2022-11-20 02:35:57 -0800

    added existing transaction implementation and qr code support

---
+++ app/src/main/java/com/group19/stashup/ui/transactions/ManualExistingFragment.kt
@@ -0,0 +1,82 @@
+package com.group19.stashup.ui.transactions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.group19.stashup.R
+import com.group19.stashup.databinding.FragmentManualExistingBinding
+import com.group19.stashup.ui.transactions.database.TransactionsViewModel
+
+class ManualExistingFragment : Fragment(), View.OnClickListener {
+
+    // Bindings
+    private var _binding: FragmentManualExistingBinding? = null
+    private val binding get() = _binding!!
+
+    // ViewModel
+    private lateinit var transactionsViewModel: TransactionsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentManualExistingBinding.inflate(inflater, container, false)
+
+        // ViewModels
+        transactionsViewModel = ViewModelProvider(this)[TransactionsViewModel::class.java]
+
+        // Set onClickListeners
+        binding.addTransactionBtn.setOnClickListener(this)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(v: View?) {
+        if (v == null) return
+
+        when (v.id) {
+            R.id.add_transaction_btn -> {
+                if (checkFields()) {
+                    binding.linearLayout.visibility = LinearLayout.GONE
+                    binding.progressBar.visibility = ProgressBar.VISIBLE
+                    transactionsViewModel.addTransactionByUid(
+                        binding.transactionIdEt.text.toString(),
+                        requireActivity()
+                    )
+                    transactionsViewModel.dataStatus().observe(viewLifecycleOwner) {
+                        if (!it) return@observe
+
+                        binding.progressBar.visibility = ProgressBar.GONE
+                        binding.linearLayout.visibility = LinearLayout.VISIBLE
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkFields(): Boolean {
+        val tUid = binding.transactionIdEt.text.toString()
+
+        if (tUid.isEmpty()) {
+            binding.transactionIdEt.error = "Transaction ID can not be empty."
+            binding.transactionIdEt.requestFocus()
+        }
+
+        if (tUid.isNotEmpty()) {
+            return true
+        }
+
+        return false
+    }
+}
--- app/src/main/java/com/group19/stashup/ui/transactions/ViewTransactionFragment.kt
+++ app/src/main/java/com/group19/stashup/ui/transactions/ViewTransactionFragment.kt
@@ -2,9 +2,7 @@ package com.group19.stashup.ui.transactions
 
 import android.content.DialogInterface
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
@@ -12,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
+import com.group19.stashup.MainViewModel
 import com.group19.stashup.R
 import com.group19.stashup.databinding.FragmentViewTransactionBinding
 import com.group19.stashup.ui.transactions.database.PersonListViewAdapter
@@ -26,6 +25,7 @@ class ViewTransactionFragment : Fragment(), View.OnClickListener {
 
     private lateinit var transaction: Transaction
     private lateinit var transactionsViewModel: TransactionsViewModel
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var currencyCode: String
     private lateinit var people: ArrayList<String>
 
@@ -35,6 +35,11 @@ class ViewTransactionFragment : Fragment(), View.OnClickListener {
     ): View {
         _binding = FragmentViewTransactionBinding.inflate(inflater, container, false)
         transaction = requireArguments().getParcelable("transaction")!!
+
+        // Add overflow menu.
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        mainViewModel.menuId.postValue(R.menu.view_transaction_menu)
+        mainViewModel.transaction = transaction
 
         transactionsViewModel = ViewModelProvider(this)[TransactionsViewModel::class.java]
 
@@ -123,7 +128,12 @@ class ViewTransactionFragment : Fragment(), View.OnClickListener {
                 )
 
                 var yourCost =
-                    "-$ ${String.format("%.2f", transaction.cost / transaction.people.size)} $currencyCode"
+                    "-$ ${
+                        String.format(
+                            "%.2f",
+                            transaction.cost / transaction.people.size
+                        )
+                    } $currencyCode"
                 if (transaction.ownerUid == transaction.payerUid) {
                     binding.payReceiveTv.setTextColor(requireActivity().getColor(R.color.green))
                     yourCost = "+$ ${
