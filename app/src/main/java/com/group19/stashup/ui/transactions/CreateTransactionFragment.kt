bc066830c52b5757ed09ddf96a82c12e47baded8
michaelzhu <mjz@sfu.ca>
2022-12-01 23:17:34 -0800

    implemented spinner used to determine categories

--- app/src/main/java/com/group19/stashup/ui/transactions/CreateTransactionFragment.kt
+++ app/src/main/java/com/group19/stashup/ui/transactions/CreateTransactionFragment.kt
@@ -61,6 +61,10 @@ class CreateTransactionFragment : Fragment(), View.OnClickListener,
      * Initialize views in layouts.
      */
     private fun initializeViewsInLayout() {
+        // Populate spinner.
+        val adapter = ArrayAdapter.createFromResource(requireActivity(), R.array.expense_categories, R.layout.spinner_layout)
+        binding.spinner.adapter = adapter
+
         // Set onClickListeners.
         binding.sharedCb.setOnCheckedChangeListener(this)
         binding.setLocationCv.setOnClickListener(this)
@@ -83,6 +87,7 @@ class CreateTransactionFragment : Fragment(), View.OnClickListener,
         if (this::transaction.isInitialized) {
             transactionsViewModel.transactionName = transaction.transactionName
             transactionsViewModel.cost = transaction.cost
+            transactionsViewModel.category = transaction.category
             transactionsViewModel.city = transaction.city
             transactionsViewModel.country = transaction.country
             transactionsViewModel.isShared = transaction.isShared
@@ -91,6 +96,7 @@ class CreateTransactionFragment : Fragment(), View.OnClickListener,
             if (transaction.ownerUid != transaction.payerUid) {
                 binding.nameEt.isEnabled = false
                 binding.costEt.isEnabled = false
+                binding.spinner.isEnabled = false
                 binding.setLocationCv.isClickable = false
                 binding.sharedCb.isClickable = false
                 binding.payCb.isClickable = false
@@ -275,6 +281,7 @@ class CreateTransactionFragment : Fragment(), View.OnClickListener,
         val transaction = Transaction().apply {
             transactionName = binding.nameEt.text.toString().trim()
             cost = binding.costEt.text.toString().trim().toDouble()
+            category = binding.spinner.selectedItem.toString()
             isShared = binding.sharedCb.isChecked
             ownerUid = transactionsViewModel.uid
             payerUid = payUid
